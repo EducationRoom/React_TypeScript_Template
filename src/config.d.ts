61396f406e477c0d1582eabd617840d515be065d
Stanley Jin <jinwangjitai@outlook.com>
2020-10-05 18:06:22 +1100

    feature: add svg loader

---
+++ src/config.d.ts
@@ -0,0 +1,10 @@
+declare module '*.png'
+declare module '*.svg'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module 'react-router-dom'
+declare module 'i18next'
+declare module '*.json' {
+  const value: any
+  export default value
+}
